2f7073db861e1a5db2ef9ded3fe18cfa78c9f366
Sean Kane <spkane31@gmail.com>
2023-09-07 17:32:49 -0400

    i think i got the cors right this time, works on my machine

--- web-app/pages/api/data.js
+++ web-app/pages/api/data.js
@@ -1,7 +1,33 @@
 // not the greatest (or even good) API design but hey it works
 
-export default function data(req, res) {
-  res.status(200).json({
+import Cors from "cors";
+
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
+  // Rest of the API logic
+  res.json({
     final_standings: [
       [
         "Team 1",
