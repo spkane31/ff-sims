dbb318d27c9a875b2b97c9ad29f285b47710bc29
Sean Kane <spkane31@gmail.com>
2024-08-28 14:27:36 -0600

    add the Data page as a filler

---
+++ frontend/pages/data/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Create a functional component for the page
+const Data = () => {
+  return (
+    <div>
+      <h1>Coming Soon</h1>
+    </div>
+  );
+};
+
+export default Data;
