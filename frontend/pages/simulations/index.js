7dbdbc0bb56fbf657ebf33c58763600806be85a9
Sean Kane <spkane31@gmail.com>
2024-08-28 15:29:37 -0600

    adding more data to simulations table page

--- frontend/pages/simulations/index.js
+++ frontend/pages/simulations/index.js
@@ -2,43 +2,84 @@ import * as React from "react";
 import schedule from "../../data/schedule.json";
 import team_avgs from "../../data/team_avgs.json";
 import { DataGrid } from "@mui/x-data-grid";
-import { Box, Paper } from "@mui/material";
+import {
+  Box,
+  Paper,
+  Head,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 export default function Home() {
   return (
-    <>
-      <h1>Simulations</h1>
+    <Box
+      sx={{
+        padding: "5rem 0",
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        overflowX: "auto",
+        paddingLeft: "5%",
+        paddingRight: "5%",
+      }}
+    >
       <TeamData />
+      <Box sx={{ marginTop: "25px" }} />
       <Schedule />
-    </>
+    </Box>
   );
 }
 
 const Schedule = () => {
   return (
-    <ul>
-      {schedule.map((week, weekIdx) => (
-        <li key={weekIdx}>
-          <h2>Week {weekIdx + 1}</h2>
-          <ul>
-            {week.map((game, gameIdx) => (
-              <TeamMatchup game={game} key={gameIdx} />
+    <>
+      <Typography variant="h5" sx={{ textAlign: "center" }}>
+        Season Prediction
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell>Home Team</TableCell>
+              <TableCell>Home Win Percentage</TableCell>
+              <TableCell>Away Team</TableCell>
+              <TableCell>Away Win Percentage</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {schedule.map((week, weekIdx) => (
+              <React.Fragment key={weekIdx}>
+                <TableRow>
+                  <TableCell colSpan={4} align="center">
+                    <Typography variant="h6">Week {weekIdx + 1}</Typography>
+                  </TableCell>
+                </TableRow>
+                {week.map((game, gameIdx) => (
+                  <TeamMatchup game={game} key={gameIdx} />
+                ))}
+              </React.Fragment>
             ))}
-          </ul>
-        </li>
-      ))}
-    </ul>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
-const TeamMatchup = ({ game, numSimulations = 100 }) => {
-  // const [homeWins, setHomeWins] = React.useState(0);
-  // const [awayWins, setAwayWins] = React.useState(0);
-
+const TeamMatchup = ({ game, numSimulations = 500 }) => {
   const { average: home_average, std_dev: home_std_dev } =
     team_avgs[game.home_team_owner];
   const { average: away_average, std_dev: away_std_dev } =
     team_avgs[game.away_team_owner];
+  const { average: league_average, std_dev: league_std_dev } =
+    team_avgs["League"];
 
   const runMonteCarloSimulation = (
     home_average,
@@ -51,15 +92,25 @@ const TeamMatchup = ({ game, numSimulations = 100 }) => {
     let awayWins = 0;
 
     for (let i = 0; i < numSimulations; i++) {
-      const homeScore = Math.random() * home_std_dev + home_average;
-      const awayScore = Math.random() * away_std_dev + away_average;
+      const jitterPercentageHome = Math.random() * 0.1 + 0.05;
+      const jitterPercentageAway = Math.random() * 0.1 + 0.05;
+
+      const leagueJitterHome = Math.random() * league_std_dev + home_average;
+      const leagueJitterAway = Math.random() * league_std_dev + away_average;
+
+      const homeScore =
+        (1 - jitterPercentageHome) * Math.random() * home_std_dev +
+        home_average +
+        jitterPercentageHome * leagueJitterHome;
+      const awayScore =
+        (1 - jitterPercentageAway) * Math.random() * away_std_dev +
+        away_average +
+        jitterPercentageAway * leagueJitterAway;
 
       if (homeScore > awayScore) {
         homeWins++;
-        // setHomeWins(homeWins + 1);
       } else {
         awayWins++;
-        // setAwayWins(awayWins + 1);
       }
     }
     return { homeWins, awayWins };
@@ -75,25 +126,13 @@ const TeamMatchup = ({ game, numSimulations = 100 }) => {
   const awayWinPercentage = awayWins / (homeWins + awayWins);
   const homeWinPercentage = homeWins / (homeWins + awayWins);
 
-  // TODO seankane: this function is AI created but it looks wonky, add this later
-  const convertToBettingOdds = (percentage) => {
-    const decimalOdds = percentage; //100 / percentage;
-    const americanOdds =
-      decimalOdds > 2
-        ? Math.round((decimalOdds - 1) * 100)
-        : -Math.round(100 / (decimalOdds - 1));
-    const adjustedOdds = Math.round(americanOdds * 1.05);
-    return adjustedOdds > 0 ? `+${adjustedOdds}` : adjustedOdds.toString();
-  };
-
-  // const homeBettingOdds = convertToBettingOdds(homeWinPercentage);
-  // const awayBettingOdds = convertToBettingOdds(awayWinPercentage);
-
   return (
-    <li>
-      {game.away_team_owner} ({(100 * awayWinPercentage).toFixed(3)} %) @{" "}
-      {game.home_team_owner} ({(100 * homeWinPercentage).toFixed(3)} %)
-    </li>
+    <TableRow>
+      <TableCell>{game.home_team_owner}</TableCell>
+      <TableCell>{(100 * homeWinPercentage).toFixed(3)}%</TableCell>
+      <TableCell>{game.away_team_owner}</TableCell>
+      <TableCell>{(100 * awayWinPercentage).toFixed(3)}%</TableCell>
+    </TableRow>
   );
 };
 
@@ -102,15 +141,26 @@ const TeamData = () => {
     { field: "teamName", headerName: "Team Name", flex: 1 },
     { field: "average", headerName: "Average", flex: 1 },
     { field: "std_dev", headerName: "Standard Deviation", flex: 1 },
+    { field: "projected_wins", headerName: "Projected Wins", flex: 1 },
+    { field: "projected_losses", headerName: "Projected Losses", flex: 1 },
+    { field: "playoff_odds", headerName: "Playoff Odds", flex: 1 },
+    { field: "last_place_odds", headerName: "Last Place Odds", flex: 1 },
   ];
 
   const rows = Object.entries(team_avgs).map(
-    ([teamName, { average, std_dev }]) => ({
-      id: teamName,
-      teamName,
-      average: average.toFixed(2),
-      std_dev: std_dev.toFixed(2),
-    })
+    ([teamName, { average, std_dev }]) => {
+      const wins = Math.random() * 14;
+      return {
+        id: teamName,
+        teamName,
+        average: average.toFixed(2),
+        std_dev: std_dev.toFixed(2),
+        projected_wins: wins.toFixed(2),
+        projected_losses: (14 - wins).toFixed(2),
+        playoff_odds: Math.random().toFixed(2),
+        last_place_odds: Math.random().toFixed(2),
+      };
+    }
   );
 
   return (
@@ -122,10 +172,13 @@ const TeamData = () => {
     >
       <Paper
         sx={{
-          minWidth: 500,
+          minWidth: 750,
           minHeight: 400,
         }}
       >
+        <Typography variant="h5" sx={{ textAlign: "center" }}>
+          Team Scoring Data
+        </Typography>
         <DataGrid
           columns={columns}
           rows={rows}
