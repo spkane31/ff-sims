2dbcf7770cad0e535065e501ac1e1c60f5fbf231
Sean Kane <spkane31@gmail.com>
2024-09-01 11:37:32 -0600

    styling the schedule page

--- frontend/pages/schedule/index.js
+++ frontend/pages/schedule/index.js
@@ -9,7 +9,6 @@ import {
   TableHead,
   TableRow,
   Box,
-  Button,
 } from "@mui/material";
 import Simulator from "../../simulation/simulator";
 import { normalDistribution } from "../../utils/math";
@@ -17,9 +16,6 @@ import teamAvgs from "../../data/team_avgs.json";
 
 const Schedule = () => {
   const [simulator, setSimulator] = React.useState(null);
-  const [teamData, setTeamData] = React.useState(null);
-  const [steps, setSteps] = React.useState(25000);
-  const [totalRunTime, setTotalRunTime] = React.useState(0);
   const [schedule, setSchedule] = React.useState(null);
 
   React.useEffect(() => {
@@ -50,45 +46,10 @@ const Schedule = () => {
         paddingRight: "5%",
       }}
     >
-      <Box>
-        {/* <Button
-          onClick={() => {
-            const start = new Date().getTime();
-            for (let i = 0; i < steps; i++) {
-              simulator.step();
-            }
-            const end = new Date().getTime();
-            setTotalRunTime(totalRunTime + (end - start));
-            setTeamData(simulator.getTeamScoringData());
-          }}
-          variant="contained"
-          sx={{ marginRight: "10px" }}
-        >
-          Simulate (25,000)
-        </Button> */}
-        {/* <Button
-          onClick={() => {
-            setSimulator(new Simulator());
-            setTotalRunTime(0);
-          }}
-          variant="contained"
-          sx={{ marginLeft: "10px" }}
-        >
-          Reset
-        </Button> */}
-      </Box>
-      {/* <Box sx={{ marginTop: "15px" }} />
-      <Typography variant="h6" sx={{ textAlign: "center" }}>
-        Total Run Time: {totalRunTime}ms (N ={" "}
-        {simulator.simulations.toLocaleString()})
+      <Box sx={{ marginTop: "15px" }} />
+      <Typography variant="h4" sx={{ textAlign: "center" }}>
+        Schedule
       </Typography>
-      <Typography variant="h6" sx={{ textAlign: "center" }}>
-        Average Run Time:{" "}
-        {totalRunTime === 0
-          ? "-"
-          : (totalRunTime / simulator.simulations).toFixed(3)}{" "}
-        ms
-      </Typography> */}
       <Box sx={{ marginTop: "15px" }} />
       <ScheduleTable
         schedule={schedule}
@@ -106,40 +67,36 @@ const ScheduleTable = ({ schedule, simulator }) => {
   }
 
   return (
-    <>
-      <Typography variant="h5" sx={{ textAlign: "center" }}>
-        Schedule
-      </Typography>
-
-      <TableContainer component={Paper}>
-        <Table stickyHeader size="small">
-          <TableHead>
-            <TableRow rowHeight={25}>
-              <TableCell>Home Team</TableCell>
-              <TableCell>Home Win Percentage</TableCell>
-              <TableCell>Home Score</TableCell>
-              <TableCell align="right">Away Score</TableCell>
-              <TableCell align="right">Away Win Percentage</TableCell>
-              <TableCell align="right">Away Team</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {schedule.map((week, weekIdx) => (
-              <React.Fragment key={weekIdx}>
-                <TableRow rowHeight={25}>
-                  <TableCell colSpan={6} align="center">
-                    <Typography variant="h6">Week {weekIdx + 1}</Typography>
-                  </TableCell>
-                </TableRow>
-                {week.map((game, gameIdx) => (
-                  <TeamMatchup game={game} teamAvgs={teamAvgs} key={gameIdx} />
-                ))}
-              </React.Fragment>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+    <TableContainer component={Paper}>
+      <Table stickyHeader size="small">
+        <TableHead>
+          <TableRow rowHeight={25}>
+            <TableCell>Home Team</TableCell>
+            <TableCell>Win Percentage</TableCell>
+            <TableCell>Projected Score</TableCell>
+            <TableCell>Score</TableCell>
+            <TableCell align="right">Score</TableCell>
+            <TableCell align="right">Win Percentage</TableCell>
+            <TableCell align="right">Projected Team</TableCell>
+            <TableCell align="right">Awat Team</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {schedule.map((week, weekIdx) => (
+            <React.Fragment key={weekIdx}>
+              <TableRow rowHeight={25}>
+                <TableCell colSpan={8} align="center">
+                  <Typography variant="h6">Week {weekIdx + 1}</Typography>
+                </TableCell>
+              </TableRow>
+              {week.map((game, gameIdx) => (
+                <TeamMatchup game={game} teamAvgs={teamAvgs} key={gameIdx} />
+              ))}
+            </React.Fragment>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
@@ -218,6 +175,8 @@ const TeamMatchup = ({ game, teamAvgs, numSimulations = 1000 }) => {
       <TableCell>{game.home_team_owner}</TableCell>
       <TableCell>{(100 * homeWinPercentage).toFixed(2)}%</TableCell>
       <TableCell>{homePoints.toFixed(2)}</TableCell>
+      <TableCell></TableCell>
+      <TableCell></TableCell>
       <TableCell align="right">{awayPoints.toFixed(2)}</TableCell>
       <TableCell align="right">
         {(100 * awayWinPercentage).toFixed(2)}%
