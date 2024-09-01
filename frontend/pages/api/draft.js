8750419f576f4afa7f2697fbf90b449b39bca986
Sean Kane <spkane31@gmail.com>
2024-09-01 11:46:47 -0600

    fixing a botched merge conflict

--- frontend/pages/api/draft.js
+++ frontend/pages/api/draft.js
@@ -4,10 +4,3 @@ import draft_data from "../../data/draft_data.json";
 export default function hello(req, res) {
   res.status(200).json(draft_data);
 }
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import draft_data from "../../data/draft_data.json";
-
-export default function hello(req, res) {
-  res.status(200).json(draft_data);
-}
