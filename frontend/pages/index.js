7eab6840cea5aa166bbc5382884f77a30abf90ad
Sean Kane <spkane31@gmail.com>
2024-09-13 17:03:42 -0600

    adding xWins

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,18 +3,61 @@ import Head from "next/head";
 import { Box, Typography } from "@mui/material";
 import Table from "../components/Table";
 import TitleComponent from "../components/TitleComponent";
+import Simulator from "../simulation/simulator";
 
 const paddingAmount = "15px";
 
 export default function Home() {
   const [historicalData, setHistoricalData] = React.useState([]);
   const [current, setCurrent] = React.useState([]);
+  const [schedule, setSchedule] = React.useState(null);
+  const [simulator, setSimulator] = React.useState(null);
+  const [teamStats, setTeamStats] = React.useState(null);
+
+  React.useEffect(() => {
+    if (teamStats !== null && schedule !== null) {
+      let sim = new Simulator(teamStats, schedule);
+      let xWins = sim.expectedWins();
+      // combine the expected wins with the current standings
+      let currentStandings = current
+        .map((team) => {
+          console.log(team);
+          let expectedWins = xWins[team.name];
+          return {
+            ...team,
+            expectedWins: xWins.find((x) => x.id === team.id).wins,
+          };
+        })
+        .sort((a, b) => {
+          return b.expectedWins - a.expectedWins;
+        });
+      setCurrent(currentStandings);
+    }
+  }, [teamStats, schedule]);
+
+  React.useEffect(() => {
+    fetch("/api/teams")
+      .then((res) => res.json())
+      .then((data) => {
+        setTeamStats(data);
+      });
+  }, []);
 
   React.useEffect(() => {
     async function fetchData() {
       const response = await fetch("/api/historical");
       const data = await response.json();
       setHistoricalData(data);
+    }
+
+    fetchData();
+  }, []);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("/api/schedule");
+      const data = await response.json();
+      setSchedule(data);
     }
 
     fetchData();
--- frontend/utils/math.js
+++ frontend/utils/math.js
@@ -5,7 +5,6 @@
  * @param {number} std - The standard deviation of the distribution.
  * @returns {number} - A random number from the normal distribution.
  */
-//
 export const normalDistribution = (mean, std) => {
   let u = 0,
     v = 0;
@@ -14,4 +13,21 @@ export const normalDistribution = (mean, std) => {
   return (
     mean + std * Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
   );
+};
+
+export const shuffle = (array) => {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
 };
