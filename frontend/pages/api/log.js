3456bba4c8271ea7a7339d5805d53d47f805fe70
Sean Kane <spkane31@gmail.com>
2024-09-19 16:47:36 -0600

    one last fix

--- frontend/pages/api/log.js
+++ frontend/pages/api/log.js
@@ -23,7 +23,7 @@ export default async function log(req, res) {
       new Date(),
     ]);
     client.release();
-    res.status(200).json(parsedResp);
+    res.status(200).json(resp);
   } catch (err) {
     res.status(500).json({
       message: err.message,
