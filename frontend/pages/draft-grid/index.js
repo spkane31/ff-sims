7f70875497421fbfbe2c85f9d581f63c3aed4a5e
Sean Kane <spkane31@gmail.com>
2024-11-03 19:38:22 -0700

    add color scaling to draft grid

--- frontend/pages/draft-grid/index.js
+++ frontend/pages/draft-grid/index.js
@@ -13,6 +13,19 @@ const Item = styled(Paper)(({ theme }) => ({
   }),
 }));
 
+const colorScale = [
+  "#006400", // DarkGreen
+  "#228B22", // ForestGreen
+  "#32CD32", // LimeGreen
+  "#7FFF00", // Chartreuse
+  "#ADFF2F", // GreenYellow
+  "#FFFF00", // Yellow
+  "#FFD700", // Gold
+  "#FFA500", // Orange
+  "#FF4500", // OrangeRed
+  "#8B0000", // DarkRed
+];
+
 const DraftGrid = () => {
   const [draft, setDraft] = React.useState(null);
   const [teams, setTeams] = React.useState(null);
@@ -62,19 +75,48 @@ const DraftData = ({ draftData, teams }) => {
     return teams.find((team) => team.id === teamID).owner;
   };
 
+  // TODO(2024.11.03): I want to color scale from green -> red for each draft round
+  // the best pick to the worst pick. I think I can do this by sorting by round and then total points
+  // and then assigning a color based on the index of the row in the sorted array.
+
+  const pointsPerRound = draftData.reduce((acc, curr) => {
+    if (acc[curr.round_number] === undefined) {
+      acc[curr.round_number] = [];
+    }
+    acc[curr.round_number].push(curr.total_points);
+    return acc;
+  }, {});
+
+  const groupedPointsPerRound = Object.values(pointsPerRound)
+    .map((roundPoints) => {
+      const grouped = [];
+      for (let i = 0; i < roundPoints.length; i += 10) {
+        grouped.push(roundPoints.slice(i, i + 10).sort((a, b) => b - a));
+      }
+      return grouped;
+    })
+    .flat();
+
+  console.log(groupedPointsPerRound);
+
   const rows = Object.entries(draftData)
     .map(([_, draftSelection]) => {
+      console.log(draftSelection);
       return {
         id: draftSelection.player_id,
         teamName: getTeamNameFromID(draftSelection.team_id),
         playerName: draftSelection.player_name,
-        pickNumber:
-          10 * (draftSelection.round_number - 1) + draftSelection.round_pick,
-        roundNumber: draftSelection.round_number,
-        totalPoints: 0,
+        pickNumber: 10 * (draftSelection.round - 1) + draftSelection.pick,
+        roundNumber: draftSelection.round,
+        totalPoints: draftSelection.total_points,
+        roundRanking: groupedPointsPerRound[draftSelection.round - 1].findIndex(
+          (points) => points === draftSelection.total_points
+        ),
       };
     })
     .sort((a, b) => b.projected_wins - a.projected_wins);
+
+  console.log(rows);
 
   // TODO seankane: even number rounds the rows needs to be reversed because we do a snake draft
   let newRows = [];
@@ -104,6 +146,7 @@ const DraftData = ({ draftData, teams }) => {
               maxHeight: "40px",
               alignContent: "center",
               fontSize: "14px",
+              color: "#000",
             }}
           >
             {row}
@@ -118,28 +161,34 @@ const DraftData = ({ draftData, teams }) => {
                 sx={{
                   minWidth: "90px",
                   maxWidth: "90px",
-                  minHeight: "40px",
-                  maxHeight: "40px",
+                  minHeight: "60px",
+                  maxHeight: "60px",
                   alignContent: "center",
                   fontSize: "14px",
+                  color: "#000",
                 }}
               >
                 Round {Math.ceil((index + 1) / 10)}
               </Item>
             </Grid>
           )}
-          <Grid item xs={12 / 11} key={row.id}>
+          <Grid item xs={12 / 11} key={row.playerName}>
             <Item
               sx={{
                 minWidth: "90px",
                 maxWidth: "90px",
-                minHeight: "40px",
-                maxHeight: "40px",
+                minHeight: "60px",
+                maxHeight: "60px",
                 alignContent: "center",
                 fontSize: "14px",
+                backgroundColor: colorScale[row.roundRanking],
+                color: "#000",
+                fontWeight: "bold",
               }}
             >
               {row.playerName}
+              <br />
+              {row.totalPoints}
             </Item>
           </Grid>
         </>
