9e1252293c48f21824cf94d913c0269887c9f169
Sean Kane <spkane31@gmail.com>
2024-11-06 20:57:10 -0700

    Add filtering on /players page (#41)
    * adding filtering to /players page
    
    * adding better filtering and getting all results
    
    * add vorp and position rank

--- frontend/pages/api/boxscoreplayers.js
+++ frontend/pages/api/boxscoreplayers.js
@@ -8,12 +8,12 @@ SELECT
   player_name,
   player_position,
   sum(projected_points) AS total_projected_points,
-  sum(actual_points) AS total_actual_points
+  sum(actual_points) AS total_actual_points,
+  sum(actual_points) - sum(projected_points) AS diff
 FROM box_score_players
 WHERE year = $1
 GROUP BY player_id, year, player_name, player_position
-ORDER BY total_actual_points DESC
-LIMIT 50;
+ORDER BY total_actual_points DESC;
 `;
 
 const queryAll = `
@@ -23,11 +23,16 @@ SELECT
   player_name,
   player_position,
   sum(projected_points) AS total_projected_points,
-  sum(actual_points) AS total_actual_points
+  sum(actual_points) AS total_actual_points,
+  sum(actual_points) - sum(projected_points) AS diff
 FROM box_score_players
 GROUP BY player_id, year, player_name, player_position
-ORDER BY total_actual_points DESC
-LIMIT 50;
+ORDER BY total_actual_points DESC;
+`;
+
+const countQuery = `
+SELECT count(*) as count
+FROM box_score_players;
 `;
 
 async function runQuery(year) {
@@ -54,6 +59,8 @@ export default async function box_score_players(req, res) {
     // get year from query param and default to current year if not present
     const resp = await runQuery(req.query.year);
 
+    const count = await pool.query(countQuery);
+
     const ret = resp.rows.map((row) => {
       return {
         year: parseInt(row.year),
@@ -62,10 +69,13 @@ export default async function box_score_players(req, res) {
         player_position: row.player_position,
         total_projected_points: parseFloat(row.total_projected_points),
         total_actual_points: parseFloat(row.total_actual_points),
+        diff: parseFloat(row.diff),
       };
     });
 
-    res.status(200).json(ret);
+    res
+      .status(200)
+      .json({ data: ret, total: count.rows[0].count, page_size: ret.length });
   } catch (err) {
     res.status(500).json({
       message: err.message,
--- frontend/pages/players/index.js
+++ frontend/pages/players/index.js
@@ -1,6 +1,14 @@
 import React from "react";
-import MenuItem from "@mui/material/MenuItem";
-import { Box, Paper, Typography, FormControl, Select } from "@mui/material";
+import {
+  Box,
+  Paper,
+  Typography,
+  FormControl,
+  FormControlLabel,
+  Checkbox,
+  FormLabel,
+  FormGroup,
+} from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
@@ -8,85 +16,163 @@ const columns = [
     field: "year",
     headerName: "Year",
     sortable: true,
+    description: "The year of the player's performance",
   },
   {
     field: "player_name",
     headerName: "Player",
     sortable: true,
     width: 200,
+    description: "The name of the player",
   },
   {
     field: "player_position",
     headerName: "Position",
     sortable: true,
+    description: "The position the player plays",
   },
   {
     field: "total_actual_points",
     headerName: "Points",
     type: "number",
     sortable: true,
+    description: "The total actual points scored by the player",
   },
   {
     field: "total_projected_points",
     headerName: "Projected Points",
     type: "number",
     sortable: true,
+    description: "The total projected points for the player",
   },
   {
     field: "diff",
     headerName: "Difference",
     type: "number",
     sortable: true,
+    description: "The difference between actual and projected points",
+  },
+  {
+    field: "position_rank",
+    headerName: "Position Rank",
+    type: "number",
+    sortable: true,
+    description: "The rank of the player within their position",
+  },
+  {
+    field: "vorp",
+    headerName: "VORP",
+    type: "number",
+    sortable: true,
+    description: "Value Over Replacement Player",
   },
 ];
 
 // Create a functional component for the page
 const Data = () => {
   const [players, setPlayers] = React.useState(null);
-  const [year, setYear] = React.useState("All");
+  const [playersCount, setPlayersCount] = React.useState(0);
+  const [years, setYear] = React.useState(["2024"]);
+  const [positions, setPositions] = React.useState([
+    "QB",
+    "RB",
+    "WR",
+    "TE",
+    "K",
+    "D/ST",
+  ]);
+
+  const vorpByPosition = {
+    QB: 20,
+    RB: 20,
+    WR: 20,
+    TE: 10,
+    K: 10,
+    "D/ST": 10,
+  };
 
   const getURL = (year) => {
-    if (year === "All") {
+    if (year === "All" || year === "") {
       return `/api/boxscoreplayers`;
     }
     return `/api/boxscoreplayers?year=${year}`;
   };
 
   React.useEffect(() => {
-    fetch(getURL(year))
+    fetch(getURL(years.join(",")))
       .then((res) => res.json())
       .then((data) => {
-        const dataWithID = data
+        const dataWithID = data.data
           .map((player, index) => {
             return {
               ...player,
               id: index,
-              diff: (
-                player.total_actual_points - player.total_projected_points
-              ).toFixed(2),
+              position_rank:
+                data.data
+                  .filter((p) => p.player_position === player.player_position)
+                  .sort((a, b) => b.total_actual_points - a.total_actual_points)
+                  .findIndex((p) => p.player_name === player.player_name) + 1,
+              vorp: 0,
             };
           })
           .sort((a, b) => b.diff - a.diff);
 
-        setPlayers(dataWithID);
+        const vorpValue = dataWithID.reduce((acc, player) => {
+          const position = player.player_position;
+          if (!acc[position]) {
+            acc[position] = [];
+          }
+          acc[position].push(player.total_actual_points);
+          return acc;
+        }, {});
+
+        Object.keys(vorpValue).forEach((position) => {
+          vorpValue[position].sort((a, b) => b - a);
+        });
+
+        const dataWithVORP = dataWithID
+          .map((player) => {
+            const position = player.player_position;
+            const rank = vorpByPosition[position];
+            const thresholdPoints = vorpValue[position][rank - 1] || 0;
+            return {
+              ...player,
+              vorp: player.total_actual_points - thresholdPoints,
+            };
+          })
+          .sort((a, b) => b.vorp - a.vorp);
+
+        setPlayers(dataWithVORP);
+        setPlayersCount(data.count);
       });
-  }, [year]);
-
-  const handleChange = (event) => {
-    setYear(event.target.value);
-  };
-
-  const currentYear = new Date().getFullYear();
-  const years = Array.from(
-    { length: currentYear - 2017 + 1 },
-    (_, index) => 2017 + index
-  ).reverse();
-
-  years.unshift("All");
+  }, []); // TODO seankane: add years after 2024
 
   if (players === null) {
     return <div>Loading...</div>;
   }
+
+  const handlePositionChange = (event) => {
+    if (event.target.checked) {
+      setPositions([...positions, event.target.value]);
+    } else {
+      setPositions(positions.filter((pos) => pos !== event.target.value));
+    }
+  };
+
+  const handleYearChange = (event) => {
+    // return;
+    if (event.target.checked) {
+      setYear([...years, event.target.value]);
+    } else {
+      setYear(years.filter((yr) => yr !== event.target.value));
+    }
+  };
+
+  const filteredPlayers = players
+    .filter((player) => {
+      return positions.includes(player.player_position);
+    })
+    .sort((a, b) => b.vorp - a.vorp);
 
   return (
     <Box
@@ -107,22 +193,67 @@ const Data = () => {
         <Box
           sx={{
             padding: "10px",
+            marginTop: "15px",
           }}
         >
-          <FormControl fullWidth>
-            <Select value={year} onChange={handleChange}>
-              {years.map((year) => (
-                <MenuItem key={year} value={year}>
-                  {year}
-                </MenuItem>
+          <FormControl component="fieldset">
+            <FormGroup aria-label="year" row>
+              {["2024"].map((year) => (
+                <FormControlLabel
+                  key={year}
+                  value={year}
+                  control={
+                    <Checkbox
+                      checked={years.includes(year)}
+                      onChange={handleYearChange}
+                      value="2024"
+                    />
+                  }
+                  label={year}
+                  labelPlacement="bottom"
+                />
               ))}
-            </Select>
+            </FormGroup>
+          </FormControl>
+        </Box>
+      </Paper>
+
+      <Paper
+        sx={{
+          padding: "10px",
+          marginTop: "15px",
+        }}
+      >
+        <Box sx={{ padding: "10px" }}>
+          <FormControl component="fieldset">
+            <FormGroup aria-label="year" row>
+              {["QB", "RB", "WR", "TE", "K", "D/ST"].map((pos) => (
+                <FormControlLabel
+                  key={pos}
+                  value={years}
+                  control={
+                    <Checkbox
+                      checked={positions.includes(pos)}
+                      onChange={handlePositionChange}
+                      value={pos}
+                    />
+                  }
+                  label={pos}
+                  labelPlacement="bottom"
+                />
+              ))}
+            </FormGroup>
           </FormControl>
         </Box>
       </Paper>
 
       <Box sx={{ paddingBottom: "15px" }} />
-      <DataGrid rows={players} columns={columns} rowHeight={40} hideFooter />
+      <DataGrid
+        rows={filteredPlayers}
+        columns={columns}
+        rowHeight={40}
+        hideFooter
+      />
     </Box>
   );
 };
